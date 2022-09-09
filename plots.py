f279462877d95fb24dade1114b03978980ca8af1
Sam Andersson <sam86andersson@gmail.com>
2022-09-09 02:47:13 +0200

    Update plots.py

--- plots.py
+++ plots.py
@@ -43,7 +43,7 @@ def plot(A,B, row_names, col_names, row_markers='markers+text', col_markers='mar
 					textfont_size=18)
 	fig.update_layout(title='Principal Coordinates',
 		yaxis_zeroline=True, xaxis_zeroline=True, 
-		plot_bgcolor='white', width=950, height=700)
+		plot_bgcolor='ivory', width=950, height=700)
 	fig.update_xaxes(zerolinecolor='navy')
 	fig.update_yaxes(zerolinecolor='navy')
 
